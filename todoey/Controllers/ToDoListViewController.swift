47ba8261455d35551a5c66989efe79453531d601
elliottxharris <exharris1@shockers.wichita.edu>
2019-01-22 10:50:45 -0600

    Convert to MVC design

--- todoey/Controllers/ToDoListViewController.swift
+++ todoey/Controllers/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-	var itemArray = [String]()
+	var itemArray = [Item]()
 	
 	let defaults = UserDefaults.standard
 	
@@ -18,7 +18,10 @@ class ToDoListViewController: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+		let newItem = Item(itemTitle: "item")
+		itemArray.append(newItem)
+		
+		if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
 			itemArray = items
 		}
 		
@@ -33,7 +36,11 @@ class ToDoListViewController: UITableViewController {
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
 		
-		cell.textLabel?.text = itemArray[indexPath.row]
+		let item = itemArray[indexPath.row]
+		
+		cell.textLabel?.text = item.title
+		
+		cell.accessoryType = item.done ? .checkmark : .none
 		
 		return cell
 	}
@@ -42,11 +49,9 @@ class ToDoListViewController: UITableViewController {
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		
-		if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .none
-		} else {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-		}
+		itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+		
+		tableView.reloadData()
 		
 		tableView.deselectRow(at: indexPath, animated: true)
 	}
@@ -59,7 +64,7 @@ class ToDoListViewController: UITableViewController {
 		let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
 		
 		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-			self.itemArray.append(textField.text!)
+			self.itemArray.append(Item(itemTitle: textField.text!))
 			
 			self.defaults.set(self.itemArray, forKey: "ToDoListArray")
 			
