68d0d50d3abf73f81dab4185664f786d715423e6
elliottxharris <exharris1@shockers.wichita.edu>
2019-02-02 14:55:27 -0600

    complete realm implementation

--- todoey/Controllers/ToDoListViewController.swift
+++ todoey/Controllers/ToDoListViewController.swift
@@ -80,6 +80,7 @@ class ToDoListViewController: UITableViewController {
 					try self.realm.write {
 						let newItem = Item()
 						newItem.title = textField.text!
+						newItem.dateCreated = Date()
 						currentCategory.items.append(newItem)
 					}
 				} catch {
@@ -109,26 +110,22 @@ class ToDoListViewController: UITableViewController {
 }
 
 //MARK: - Search Bar Methods
-//extension ToDoListViewController : UISearchBarDelegate {
-//
-//	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//		if searchBar.text?.count == 0 {
-//			loadItems()
-//
-//			DispatchQueue.main.async {
-//				searchBar.resignFirstResponder()
-//			}
-//		} else {
-//
-//			let request : NSFetchRequest<Item> = Item.fetchRequest()
-//
-//			let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-//
-//			request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-//
-//			loadItems(with: request, predicate: predicate)
-//		}
-//
-//	}
-//
-//}
+extension ToDoListViewController : UISearchBarDelegate {
+
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+		if searchBar.text?.count == 0 {
+			loadItems()
+
+			DispatchQueue.main.async {
+				searchBar.resignFirstResponder()
+			}
+		} else {
+
+			items = items?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+			
+			tableView.reloadData()
+		}
+
+	}
+
+}
